b26a83e72c6f88511b638796f01f5fe5d40aaa7e
farmaan24 <farmaanshafiq@gmail.com>
2025-04-19 15:47:56 +0530

    basic signin signup with cors

--- server/index.ts
+++ server/index.ts
@@ -2,9 +2,11 @@ import express from 'express'
 const app = express();
 
 import authRouter from './routes/auth'
+
 const cors = require("cors");
-app.use('/auth', authRouter);
 app.use(cors());
+app.use('/auth', authRouter);
+
 
 app.listen(8080, () => {
     console.log('Listening on port 8080....')
